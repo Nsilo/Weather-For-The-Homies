66a4516f0b15903c8ef6f12b5891a485c409f948
Jivaka <jivakapyane93@gmail.com>
2020-11-07 14:41:45 -0800

    UPDATE: added a function to insert the information to my html

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,38 +1,86 @@
-const searchBtn = document.getElementById("citySearchBtn")
-const cityInput = document.getElementById("cityInput")
-$("form").on("submit", function(e){
-    e.preventDefault()
-    // create a list item
-    let input = $("#cityInput").val()
-    getCurrentWeather(input)
-})
+const searchBtn = document.getElementById("citySearchBtn");
+const cityInput = document.getElementById("cityInput");
 
-function getCurrentWeather(city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=71c4b05ab2d24a5c3b0e79e073c862da")
-        .then(response => response.JSON)
 
-        .then(data => {
-            getForecast(city)
-            console.log(data);
-            getUVIndex(data.coord.lat, data.coord.lon)
+
+function formSubmitHandler(e) {
+    e.preventDefault();
+    let city = cityInput.value.trim();
+    
+    if (city) {
+        getCurrentWeather(city);
+  
+      cityInput.value = '';
+    } else {
+      
+    }
+  };
+
+function getCurrentWeather(city){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=b96b52626f78508367acbb00dd591cd2`)
+      .then(response => response.json())
+         // save city into local storage
+
+        //create html elements with response.data
+
+        // call other functions
+        
+      .then(data => {
+
+          getForecast(city);
+          // console.log(data)
+          getUVIndex(data.coord.lat, data.coord.lon)
+          
         })
-
-function getForecast(city) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=71c4b05ab2d24a5c3b0e79e073c862da")
-        .then(response => response.JSON)
-
-        .then(data => {
-            console.log(data);
-            $("#temp").text(data.value)
-        })
-
-function getUVIndex(city) {
-    fetch("http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=71c4b05ab2d24a5c3b0e79e073c862da")
-    .then(response => response.JSON)
-
-    .then(data => {
-        $("#uvIndex").text(data.value)
-        $("#uvIndex").attr("class", "safe")
-    })
-
+        //console.log("fetching")
 }
+
+function getForecast(city){
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=b96b52626f78508367acbb00dd591cd2`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+  
+  function getUVIndex(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=b96b52626f78508367acbb00dd591cd2`)
+    .then(response => response.json())
+    .then(data => {
+      $("#uvIndex").text(data.value);
+      if (data.value <= 2){
+        $("#uvIndex").attr("class", "safe");
+      } else if (data.value <= 5) {
+        $("#uvIndex").attr("class", "moderate");
+      } else if (data.value <= 7) {
+        $("#uvIndex").attr("class", "high");
+      } else if (data.value <= 10) {
+        $("#uvIndex").attr("class", "veryHigh");
+      } else if (data.value > 10)
+      $("#uvIndex").attr("class", "danger");
+    })
+    $("#card-row").empty();
+    for (let i = 0; i < data.list.length; i+= 8){
+      cardInformer(data.list[i]);
+    }
+  }
+
+  let cardInformer = function (data){
+    let iconImg = data.weather[0].icon;
+    let card = $("<div>");
+    let body = $("<div>");
+    let head = $("<h4>");
+    let icon = $("<img>");
+    let temp = $("<p>");
+    let humidity = $("<p>");
+
+    card.attr("style", "width: 160px; margin-right 10px")
+    icon.attr("src", "https://openweathermap.org/img/w/" + iconcode + ".png")
+
+
+
+  }
+  
+searchBtn.addEventListener("click", formSubmitHandler);
+
+// $("#temp").text(data.list[0].main.temp)
